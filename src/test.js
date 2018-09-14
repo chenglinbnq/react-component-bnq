d4f43abb1d41fcaa9f7da0ca59059b49ea9b078e
程林 <chenglin@chenglindeMacBook-Pro.local>
2018-09-14 22:41:01 +0800

    test demo run

---
+++ src/test.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// import './index.less';
+
+class Test extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div>Test</div>
+        );
+    }
+}
+
+Test.propTypes = {
+
+}
+
+export default Test;
